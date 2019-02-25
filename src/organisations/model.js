71805681a0085ec1e9c6fbc4b565053dcdf90699
g-div <g-div@anche.no>
2019-02-25 11:19:35 +0100

    feat(organisation): add geo & subOrganisation to JSON-LD

--- src/organisations/model.js
+++ src/organisations/model.js
@@ -1,4 +1,55 @@
 const got = require('got');
+const Boom = require('boom');
+
+const serializeOrganisationSchema = (entry) => {
+  const res = {
+    '@context': 'http://schema.org',
+    '@type': 'Organisation',
+    '@id': entry._id,
+    name: entry.name,
+    description: entry.description,
+    url: entry.website,
+    sameAs: entry.website,
+    email: entry.email,
+    telephone: entry.telephone,
+  };
+
+  const logo = entry.images.find(image => image.type === 'logo');
+  if (logo) {
+    res.logo = logo.url;
+  }
+
+  if (entry.address) {
+    res.location = {
+      '@type': 'Place',
+      address: {
+        '@type': 'PostalAddress',
+        addressCountry: 'Germany',
+        addressLocality: entry.city,
+        postalCode: entry.zipcode,
+        streetAddress: entry.address,
+      },
+    };
+  }
+
+  if (entry.location) {
+    res.location.geo = {
+      '@type': 'GeoCoordinates',
+      latitude: entry.location.coordinates[0],
+      longitude: entry.location.coordinates[1],
+    };
+  }
+
+  if (entry.venues.length !== 0) {
+    res.subOrganisation = entry.venues.map((venue) => {
+      const result = serializeOrganisationSchema(venue);
+      delete result['@context'];
+      return result;
+    });
+  }
+
+  return res;
+};
 
 module.exports = (mongoose) => {
   const { Schema } = mongoose;
@@ -107,41 +158,14 @@ module.exports = (mongoose) => {
     }
   });
 
-  async function serializeJSONLD(_id) {
-    const entry = await this.model('Organisation').findById(_id).lean({ autopopulate: true });
-    const res = {
-      '@context': 'http://schema.org',
-      '@type': 'Organisation',
-      '@id': entry._id,
-      name: entry.name,
-      description: entry.description,
-      website: entry.website,
-      sameAs: entry.website,
-      email: entry.email,
-      telephone: entry.telephone,
-    };
-
-    const logo = entry.images.find(image => image.type === 'logo');
-    if (logo) {
-      res.logo = logo.url;
+  Organisation.statics.serializeJSONLD = async function serializeJSONLD(_id) {
+    try {
+      const entry = await this.model('Organisation').findById(_id);
+      return serializeOrganisationSchema(entry);
+    } catch (err) {
+      return Boom.notFound();
     }
-
-    if (entry.address) {
-      res.location = {
-        '@type': 'Place',
-        address: {
-          '@type': 'PostalAddress',
-          addressLocality: entry.city,
-          postalCode: entry.zipcode,
-          streetAddress: entry.address,
-        },
-      };
-    }
-
-    return res;
-  }
-
-  Organisation.statics.serializeJSONLD = serializeJSONLD;
+  };
 
   return mongoose.model('Organisation', Organisation);
 };
