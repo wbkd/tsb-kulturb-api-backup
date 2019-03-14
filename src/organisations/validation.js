b24d9ef3772cb4fec6e31b66765c413470379df2
g-div <g-div@anche.no>
2019-03-14 16:06:12 +0100

    refactor(organisation): remove Joi logging

--- src/organisations/validation.js
+++ src/organisations/validation.js
@@ -116,20 +116,6 @@ const relation = {
   },
 };
 
-const failAction = async (request, h, err) => {
-  if (process.env.NODE_ENV === 'production') {
-    // In prod, log a limited error message and throw the default Bad Request error.
-    console.error('ValidationError:', err.message);
-    throw Boom.badRequest(`Invalid request payload input`);
-  } else {
-    // During development, log and respond with the full error.
-    console.error(err);
-    throw err;
-  }
-};
-
-update.failAction = failAction;
-
 module.exports = {
   search,
   find,
