d1b032453e72be10ed6b5cca3ef6bf7b399587a1
g-div <g-div@anche.no>
2019-03-26 18:11:01 +0100

    fix(OSM): add a 2. request directly to OSM

--- src/organisations/utils/osm.js
+++ src/organisations/utils/osm.js
@@ -1,4 +1,5 @@
 const got = require('got');
+const parser = require('fast-xml-parser');
 
 const getOSMData = async (entry) => {
   const response = await got('https://nominatim.openstreetmap.org/search', {
@@ -13,13 +14,36 @@ const getOSMData = async (entry) => {
     },
   });
 
-  return JSON.parse(response.body).map(result => ({
-    osmId: result.place_id,
+  const res = JSON.parse(response.body).map(result => ({
+    osmId: result.osm_id,
+    osmType: result.osm_type,
     geojson: result.geojson,
     wikidataId: result.extratags.wikidata,
-    accessibility: result.extratags.wheelchair,
+    accessibilityWheelchair: result.extratags.wheelchair,
     openingHours: result.extratags.opening_hours,
   })).shift();
+
+  if (res && res.osmId) {
+    try {
+      const osmResponse = await got(`https://www.openstreetmap.org/api/0.6/${res.osmType}/${res.osmId}`);
+      const osmData = parser.parse(osmResponse.body, {
+        attributeNamePrefix: '',
+        ignoreAttributes: false,
+      });
+      const data = Object.assign(...osmData.osm[res.osmType].tag.map(tag => ({ [tag.k]: tag.v })));
+
+      if (data['blind:description:de'] || data['blind:description:en']) {
+        res.accessibilityBlind = data['blind:description:de'] || data['blind:description:en'];
+      }
+      if (data['deaf:description:de'] || data['deaf:description:en']) {
+        res.accessibilityDeaf = data['deaf:description:de'] || data['blind:description:en'];
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return { _id: entry._id, ...res };
 };
 
 module.exports = {
